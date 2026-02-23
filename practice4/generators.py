c529c77be7bb7b02ac0601499027c8bd655e2aae
DaurenKz1 <daur.ronaldojuve77@gmail.com>
2026-02-23 21:05:01 +0500

    Generators

--- practice4/generators.py
+++ practice4/generators.py
@@ -1,4 +1,5 @@
 
+#1
 def square_numbers(num):
     for i in range(1, num+1):
         yield i * i
@@ -6,3 +7,47 @@ def square_numbers(num):
 user_in = int(input())
 for i in square_numbers(user_in):
     print(i)
+
+
+#2
+def even_numbers(n):
+    for i in range(0, n+1, 2):
+        yield i
+
+user_in1 = int(input())
+
+print(*even_numbers(user_in), sep=", ")
+
+#3
+def divisible_by_3_and_4(n):
+    for i in range(0, n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
+
+
+user_in2 = int(input())
+
+print(*divisible_by_3_and_4(user_in2), sep=",")
+
+#4
+def squares(a, b):
+    for i in range(a, b + 1):
+        yield i * i
+
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+for value in squares(a, b):
+    print(value)
+
+#5
+def countdown(n):
+    for i in range(n, -1, -1):
+        yield i
+
+
+n = int(input("Enter n: "))
+
+for number in countdown(n):
+    print(number)
